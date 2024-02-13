0517ddcb89132a190dd6aa576b5440ceb4594bdd
wyb <wyb8813@gamil.com>
2024-02-14 00:07:40 +0800

    定义了边缘服务部署的强化学习环境

--- src/placingENV.py
+++ src/placingENV.py
@@ -4,10 +4,13 @@
 # 描述: 服务部署的环境，只考虑存储和cpu
 # 作者: WangYuanbo
 # --------------------------------------------------
+import os
 from typing import Tuple
 
 import gym
 import numpy as np
+import pandas as pd
+import yaml
 from gym import spaces
 from gym.core import ActType, ObsType
 
@@ -34,29 +37,26 @@ class CustomEnv(gym.Env):
     # 定义动作空间和观察空间
     def __init__(self, config):
         super().__init__()
+        # ########系统数据######
         # 定义一个非法动作的惩罚
         self.penalty = config['penalty']
         # 定义终止时间戳
         self.end_timestamp = config['end_timestamp']
-        # 定义每个容器镜像的拉取时延
-        self.images_pulling_delay = config['images_pulling_delay']
 
         # 定义环境本身的配置，服务器数量，容器数量等。
         self.server_number = config['server_number']
         self.container_number = config['container_number']
         self.request_number = config['request_number']
-        # 除此以外，每个容器需要多少存储空间，也是环境信息
-        # 假设这个信息，可以从配置文件中读取
-        self.container_storage = config['container_storage']
-        self.container_cpu = config['container_cpu']
 
-        # 定义每个用户的请求需要多少cpu计算资源，也是环境信息
-        self.request_resource = config['request_resource']
+        # ########随机生成的数据##########
+        # 定义边缘服务器的参数存储和cpu空间
+        self.server_info = config['server_info']
 
-        # 定义每个时隙的用户请求,
-        # 每个时隙的用户请求是一个一维数组
-        # config传入的全体请求就是一个二维数组
-        self.user_request_per_ts = config['user_request_per_ts']
+        # 定义每个用户的请求,需要哪个镜像?需要多少cpu计算资源，也是环境信息
+        self.user_request_info = config['user_request_info']
+
+        # 定义每个容器镜像信息,包括id,大小,拉取时延
+        self.container_info = config['container_info']
 
         # ######定义观察空间#######
         # 1. 定义上一时刻的容器部署信息,x_{n,s}={0,1}表示服务器n上是否部署容器s
@@ -119,7 +119,7 @@ class CustomEnv(gym.Env):
         # 根据这个动作计算奖励值，
         # 如果动作是合法的，获得奖励，并更新状态
         # 如果动作是非法的，要更新时间戳状态，不更新上一个部署的状态
-        if not self.isValid(action):
+        if not self.isValid(action, state):
             reward += self.penalty
         else:
             reward += self.getReward(action, state)
@@ -128,11 +128,7 @@ class CustomEnv(gym.Env):
             state["last_request_routing"] = action["now_request_routing"]
         # 更新系统时间戳状态
         state['timestamp'] += 1
-
-        # 根据下一个时间戳更新下一个时间戳的系统状态
         ts = state['timestamp']
-        state['user_request'] = self.user_request_per_ts[ts]
-
         # 判断是否达到了截断条件和终止条件?
         if ts > self.end_timestamp:
             terminated = True
@@ -152,9 +148,10 @@ class CustomEnv(gym.Env):
         return self.state
 
     # 检查动作是不是合法的
-    def isValid(self, action: ActType) -> bool:
+    def isValid(self, action: ActType, state: ObsType) -> bool:
         x = action['now_container_place']
         y = action['now_request_routing']
+        ts = state['timestamp']
         # 约束一
         # 检查是不是所有的用户请求都有一个服务器完成
         for u in range(self.request_number):
@@ -176,16 +173,17 @@ class CustomEnv(gym.Env):
             # 计算服务器n上的存储空间
             n_storage = 0
             for s in range(self.container_number):
-                n_storage += x[n][s] * self.container_storage[s]
-            if n_storage > self.container_storage[n]:
+                n_storage += x[n][s] * self.container_info[s]['container_size']
+            if n_storage > self.server_info[n]['storage_size']:
                 return False
         # 约束四
         # 对于请求路由，首先检查服务器的cpu资源是不是足够的
         for n in range(self.server_number):
             n_cpu = 0
             for u in range(self.request_number):
-                n_cpu += y[u][n] * self.request_resource[u]
-            if n_cpu > self.container_cpu[n]:
+                col_name = 'user_' + str(u) + '_cpu'
+                n_cpu += y[u][n] * self.user_request_info[ts][col_name]
+            if n_cpu > self.server_info[n]:
                 return False
         # 需要考虑请求覆盖的问题
         # 这个约束在建模里面没有
@@ -195,7 +193,7 @@ class CustomEnv(gym.Env):
     def getReward(self, action: ActType, state: ObsType) -> int:
         # 如果当前时间戳是1，直接计算拉取延迟
         x = action['now_container_place']
-        b = self.images_pulling_delay
+        b = self.container_info['images_pulling_delay']
         total_delay = 0
         delay_pulling = 0
         if state.timestamp == 0:
@@ -204,3 +202,50 @@ class CustomEnv(gym.Env):
                     delay_pulling += x[n][s] * b[s]
         total_delay += delay_pulling
         return -total_delay
+
+
+if __name__ == '__main__':
+    print('hello')
+    with open('config.yaml', 'r', encoding='utf-8') as f:
+        config = yaml.safe_load(f)
+
+    father_directory = '../data'
+    info_file = {
+        'server_info.csv': 'server_id',
+        'container_info.csv': 'container_id',
+        'user_request_info.csv': 'timestamp',
+    }
+    # import pandas as pd
+    # 从csv文件读取数据
+    # 修改数据帧的索引
+    # # CSV文件名称
+    # csv_file = '../data/server_info.csv'  # 请用你实际的文件名替换 'your_file.csv'
+    for file_name in info_file:
+        csv_path = os.path.join(father_directory, file_name)
+        key = info_file[file_name]
+        with open(csv_path, 'r', encoding='utf-8') as f:
+            data_frame = pd.read_csv(f)
+            data_frame.set_index(key, inplace=True)
+            data_dict = data_frame.to_dict('index')
+            # print(data_dict)
+            config[file_name.replace('.csv', '')] = data_dict
+    # print(config)
+    env = CustomEnv(config=config)
+    print("hello")
+    print(env.action_space)
+    print(env.observation_space)
+    #
+    # # # 使用pandas.read_csv函数读取CSV文件
+    # # data_frame = pd.read_csv(csv_file)
+    # # # 我们将 'server_id' 列设置为数据帧的索引
+    # # data_frame.set_index('server_id', inplace=True)
+    # # # 使用pandas.DataFrame.to_dict函数将数据帧(DataFrame)转换为字典
+    # # # 这里我们使用了 'records' 参数，这会使得每行数据转换为一个字典，所有的这些字典形成列表
+    # # # 'records' 使得每条记录被表示为带有列名称的字典
+    # # data_dict = data_frame.to_dict('index')
+    # # csv_dict = {}
+    # # csv_dict[csv_file.replace('.csv', '')] = data_dict
+    # print(csv_dict)
+    # # 打印出结果字典，用于检查
+    # for item in data_dict:
+    #     print(item)
