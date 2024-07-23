b45f6c7e52c3952629f000cee24bc3e224c0e1dd
wyb <wyb8813@gamil.com>
2024-07-24 00:15:03 +0800

    init

--- auto_src/performance_test.py
+++ auto_src/performance_test.py
@@ -13,11 +13,11 @@ import torch
 import torch.nn.functional as F
 import yaml
 
+from auto_src.tools import find_solution_file
 from baseline import random_rand
 from ddpg import DDPG
 from env_with_interference import CustomEnv
 
-env = CustomEnv('cpu')
 with open('train_config.yaml', 'r', encoding='utf-8') as f:
     config_data = yaml.safe_load(f)
 
@@ -35,10 +35,12 @@ Config = namedtuple('Config',
                      'tau',
                      'lmbda',
                      'epochs',
-                     'eps'
+                     'eps',
+                     'data_dir'
                      ])
 config = Config(**config_data)
-
+dataset = 'exp2_3'
+env = CustomEnv('cpu', dataset)
 state_dim = env.state_dim
 action_dim = env.action_dim
 agent = DDPG(state_dim=state_dim, action_dim=action_dim,
@@ -46,7 +48,7 @@ agent = DDPG(state_dim=state_dim, action_dim=action_dim,
              tau=config.tau, gamma=config.gamma, device='cpu', log_dir='log')
 
 
-def eval_model_solution(agent):
+def eval_model_solution(agent, is_valid=True):
     """
     测试用深度模型的性能
     :param agent:
@@ -67,7 +69,8 @@ def eval_model_solution(agent):
 
         # 计算干扰因子
         reward, valid, factor = env.cal_placing_rewards(action, interference_factor=True)
-        assert valid.all(), 'not valid action'
+        if is_valid:
+            assert valid.all(), 'not valid action'
 
         episode_reward.append(reward.tolist())
         episode_interference.append(factor.tolist())
@@ -94,11 +97,16 @@ def LR_Instant(env: CustomEnv, relax=True):
     while not done:
         start_time = time.time()
         raw_action = torch.tensor(env.model_solve(relax))
+        time_0 = time.time()
+        print('time stamp', env.timestamp, time_0 - start_time)
         while True:
             action = random_rand(raw_action)
             _, valid = env.cal_placing_rewards(action)
             if valid.all():
                 break
+            if time.time() - time_0 > 3:
+                print(time.time() - time_0)
+                action = torch.tensor(env.model_solve(False))
         end_time = time.time()
         execution_time = end_time - start_time
         episode_time.append(execution_time)
@@ -173,12 +181,10 @@ def cloud(env: CustomEnv):
     return total_reward, episode_reward, episode_interference, episode_time
 
 
-def greedy(env: CustomEnv):
+def greedy(env: CustomEnv, trade_factor=0.0):
     """
-    按个服务器试，能放就放，不能放换一个
-    request 信息第一维是时间戳，第二维是容器id，第三维表示资源和延迟
+    按照best fit 原则，优先放置延迟大的。
     :param env:
-    :return:
     """
     total_reward = torch.zeros(env.container_number)
     episode_reward = []
@@ -188,7 +194,7 @@ def greedy(env: CustomEnv):
     state, done = env.reset()
     container_info = env.container_info
 
-    max_cap = [env.max_cpu, env.max_mem, env.max_net_in, env.max_net_out]
+    max_cap = torch.tensor([env.max_cpu, env.max_mem, env.max_net_in, env.max_net_out])
     while not done:
         server_cap = copy.deepcopy(env.server_info)
         ts = env.timestamp
@@ -199,8 +205,7 @@ def greedy(env: CustomEnv):
             for server_id in range(env.server_number):
                 tag = True
                 for j in range(4):
-                    if server_cap[server_id][j] < request[j] or server_cap[server_id][j] <= 0.3 * (max_cap[j]):
-                        # if server_cap[server_id][j] < request[j]:
+                    if server_cap[server_id][j] < request[j] or server_cap[server_id][j] <= trade_factor * max_cap[j]:
                         tag = False
                         break
                 # 检测存储
@@ -231,11 +236,73 @@ def greedy(env: CustomEnv):
     return total_reward, episode_reward, episode_interference, episode_time
 
 
+def greedy_place(env: CustomEnv, trade_factor=0.0):
+    """
+    按照 最大剩余优先 原则，优先放置延迟大的。
+    :param env:
+    """
+    total_reward = torch.zeros(env.container_number)
+    episode_reward = []
+    episode_interference = []
+    episode_time = []
+
+    state, done = env.reset()
+    container_info = env.container_info
+
+    max_cap = [env.max_cpu, env.max_mem, env.max_net_in, env.max_net_out]
+    while not done:
+        server_cap = copy.deepcopy(env.server_info).tolist()
+        for id, ele in enumerate(server_cap):
+            ele.append(id)
+
+        ts = env.timestamp
+        routing_id = [env.server_number] * env.container_number
+        start_time = time.time()
+
+        for i in range(env.user_number):
+            request = env.user_request_info[ts][i]
+            server_cap = sorted(server_cap, key=lambda x: sum([x[i] / max_cap[i] for i in range(4)]), reverse=True)
+            for id in range(env.server_number):
+                server_id = server_cap[id][-1]
+                tag = True
+                for j in range(4):
+                    if server_cap[id][j] < request[j] or server_cap[id][j] <= trade_factor * max_cap[j]:
+                        tag = False
+                        break
+                # 检测存储
+                if server_cap[id][4] < container_info[i][0]:
+                    tag = False
+                if tag:  # 证明当前服务器放得下
+                    for j in range(4):
+                        server_cap[id][j] -= request[j]
+                        routing_id[i] = server_id
+                    server_cap[id][4] -= container_info[i][0]
+                    break
+
+        action = torch.tensor(routing_id).long()
+        action = F.one_hot(action, num_classes=env.server_number + 1)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        episode_time.append(execution_time)
+
+        # 计算干扰因子
+        reward, valid, factor = env.cal_placing_rewards(action, interference_factor=True)
+        assert valid.all(), 'not valid action'
+
+        episode_reward.append(reward.tolist())
+        episode_interference.append(factor.tolist())
+        total_reward += reward
+
+        state, reward, done, info = env.step(action)
+    total_reward = torch.sum(total_reward).item()
+    return total_reward, episode_reward, episode_interference, episode_time
+
+
 data = {
-    'TD3': [],
+
     'DDPG': [],
-    'BC-TD3': [],
     'BC-DDPG': [],
+    'TD3': [],
     'BC': [],
     'LR-Instant': [],
     'JSPRR': [],
@@ -243,39 +310,44 @@ data = {
     'Cloud': []
 }
 
-# bc-td3
-actor_para_path = r'log_res/imitation_learning_td3_env-20240720-094656/itd3_actor.pth'
-agent.actor.load_state_dict(torch.load(actor_para_path))
-bc_td3 = eval_model_solution(agent)
+father_dir = r'log_res/' + dataset
+
+_ = find_solution_file(father_dir)
+ddpg, bc_ddpg, td3, bc = _
 
 # td3
-actor_para_path = r'log_res/td3_agent_env-20240720-005856/td3_actor.pth'
-agent.actor.load_state_dict(torch.load(actor_para_path))
+agent.actor.load_state_dict(torch.load(td3))
 td3 = eval_model_solution(agent)
-
-
+print('td3 complete')
 # ddpg
-actor_para_path = r'log_res/ddpg_agent_ddpg-20240720-012731/ddpg_actor.pth'
-agent.actor.load_state_dict(torch.load(actor_para_path))
+agent.actor.load_state_dict(torch.load(ddpg))
 ddpg = eval_model_solution(agent)
+print('ddpg complete')
 
 # bc-ddpg
-actor_para_path = r'log_res/imitation_learning_env-20240720-100544/bc_ddpg_actor.pth'
-agent.actor.load_state_dict(torch.load(actor_para_path))
+agent.actor.load_state_dict(torch.load(bc_ddpg))
 bc_ddpg = eval_model_solution(agent)
+print('bc ddpg complete')
 
 # bc
-actor_para_path = r'model_para/best_actor.pth'
-agent.actor.load_state_dict(torch.load(actor_para_path))
-bc = eval_model_solution(agent)
+agent.actor.load_state_dict(torch.load(bc))
+bc = eval_model_solution(agent, False)
+print('bc complete')
+#
+greedy_res = greedy_place(env, )
+print(greedy_res[0])
+print('greedy complete')
 
-greedy_res = greedy(env)
 jsprr_res = JSPRR(env)
+print('jsprr complete')
+#
 lr_ins = LR_Instant(env)
+print('lr instant complete')
+
 cloud_res = cloud(env)
+print('cloud complete')
 
 data['TD3'] = td3
-data['BC-TD3'] = bc_td3
 data['DDPG'] = ddpg
 data['BC-DDPG'] = bc_ddpg
 data['BC'] = bc
@@ -285,6 +357,8 @@ data['LR-Instant'] = lr_ins
 data['Greedy'] = greedy_res
 for key, value in data.items():
     print(key, value[0])
+
 # 将数据写入JSON文件
-with open("model_para/performance_result.pkl", "wb") as file:
+dir = r'performance_res/' + dataset + '_compare_res.pkl'
+with open(dir, "wb") as file:
     pickle.dump(data, file)
